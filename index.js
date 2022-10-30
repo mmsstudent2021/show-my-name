ab9e58f7601c1ff4151cf2f9f41de1e05a1e008b
mmsstudent2021 <mmsstudent2021@gmail.com>
2022-10-30 10:49:04 +0630

    source

---
+++ index.js
@@ -0,0 +1,18 @@
+const myName = "hein htet zan";
+
+export function showMyName() {
+    return myName;
+}
+
+export function showMyNameUI(){
+    const h1 = document.createElement("h1");
+    h1.innerText = myName;
+    h1.style.position = 'fixed';
+    h1.style.zIndex = 10000;
+    h1.style.fontSize = 100+"px";
+    h1.style.fontWeight = "blod";
+    h1.style.top = 0;
+    h1.style.right = 0;
+    h1.style.backgroundColor = "black";
+    document.body.append(h1);
+}
