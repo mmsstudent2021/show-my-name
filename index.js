504653980e252fa23b167d4527df98f79cac08a2
mmsstudent2021 <mmsstudent2021@gmail.com>
2022-10-30 10:56:39 +0630

    update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ export function showMyNameUI(){
     h1.style.position = 'fixed';
     h1.style.zIndex = 10000;
     h1.style.fontSize = 100+"px";
-    h1.style.fontWeight = "blod";
+    h1.style.fontWeight = "bold";
     h1.style.top = 0;
     h1.style.right = 0;
     h1.style.backgroundColor = "black";
